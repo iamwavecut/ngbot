50bb8e72ad74b44c2567664519cdfff7a02a23e5
Valeriy Selitskiy <wavecutdnb@gmail.com>
2020-03-06 09:32:37 +0300

    Charades WIP

--- handlers/charades.go
+++ handlers/charades.go
@@ -15,23 +15,29 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 const charadeShowWord = "CHARADE_SHOW_WORD"
-const charadeOptOut = "CHARADE_OPT_OUT"
+const charadeAnotherWord = "CHARADE_ANOTHER_WORD"
+const charadeContinue = "CHARADE_CONTINUE"
 
-var charadeCallbackData = []string{charadeShowWord, charadeOptOut}
+var charadeCallbackData = []string{charadeShowWord, charadeAnotherWord, charadeContinue}
 
 type act struct {
-	userID int
-	word   string
+	userID     int
+	winnerID   *int
+	word       string
+	startTime  *time.Time
+	finishTime *time.Time
 }
 
 type Charades struct {
 	s      bot.Service
 	path   string
 	data   map[string]map[string]string
-	active map[int64]act
+	active map[int64]*act
 }
 
 func NewCharades(s bot.Service) *Charades {
@@ -39,7 +45,7 @@ func NewCharades(s bot.Service) *Charades {
 		s:      s,
 		path:   infra.GetResourcesDir("charades"),
 		data:   make(map[string]map[string]string),
-		active: make(map[int64]act),
+		active: make(map[int64]*act),
 	}
 	return c
 }
@@ -56,54 +62,101 @@ func (c *Charades) Handle(u *tgbotapi.Update, cm *db.ChatMeta) (proceed bool, er
 		return true, nil
 	}
 
-	if u.Message == nil {
-		return true, nil
-	}
-	m := u.Message
 	b := c.s.GetBot()
+	m := u.Message
+	cb := u.CallbackQuery
+	userAct := c.active[cm.ID]
 
-	_, active := c.active[cm.ID]
-	if m.IsCommand() && m.Command() == "charade" {
+	if cb != nil && isValidCharadeCallback(cb) {
+		log.Trace("charade valid")
+		answer := tgbotapi.CallbackConfig{
+			CallbackQueryID: cb.ID,
+		}
+
+		if userAct != nil && cb.From.ID != userAct.userID {
+			answer.Text = i18n.Get("It's not your turn!", cm.Language)
+			b.Request(answer)
+			return false, nil
+		}
+
+		switch cb.Data {
+		case charadeShowWord:
+			if userAct == nil {
+				break
+			}
+			answer.Text = i18n.Get("It's not your turn!", cm.Language)
+			if cb.From.ID == userAct.userID {
+				answer.Text = userAct.word
+				answer.ShowAlert = true
+			}
+
+		case charadeAnotherWord:
+			if userAct == nil {
+				break
+			}
+			answer.Text = i18n.Get("It's not your turn!", cm.Language)
+			if cb.From.ID == userAct.userID {
+				userAct, err := c.randomActForUser(cb.From.ID, cm)
+				if err != nil {
+					log.WithError(err).Error("cant get act")
+				}
+				c.active[cm.ID] = userAct
+				answer.Text = userAct.word
+				answer.ShowAlert = true
+			}
+
+		case charadeContinue:
+			switch {
+			case userAct == nil, userAct.userID == cb.From.ID, userAct.userID != cb.From.ID && time.Now().Unix()-(*userAct.finishTime).Unix() >= 5:
+				err = c.startCharade(cb.From, cm)
+				if err != nil {
+					log.WithError(err).Error("cant start charade continue")
+				}
+
+			case userAct != nil && userAct.userID != cb.From.ID && time.Now().Unix()-(*userAct.finishTime).Unix() < 5:
+				answer.Text = i18n.Get("It's not your turn!", cm.Language)
+			}
+		}
+		b.Request(answer)
+		return false, nil
+	}
+
+	if userAct != nil && m != nil && !m.IsCommand() {
+		msg := tgbotapi.NewMessage(cm.ID, i18n.Get("Draw", cm.Language))
+		msg.ParseMode = "markdown"
+		kb := tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				tgbotapi.NewInlineKeyboardButtonData(i18n.Get("I want to continue", cm.Language), charadeContinue),
+			),
+		)
+		msg.ReplyMarkup = kb
+
+		if strings.ToLower(strings.Trim(m.Text, " \n\r?!.,;!@#$%^&*()/][\\")) == userAct.word {
+			if m.From.ID == userAct.userID {
+			} else {
+				msg.Text = i18n.Get("*%s makes the right guess*!", cm.Language)
+				userAct.finishTime = func() *time.Time { t := time.Now(); return &t }()
+			}
+
+			b.Send(msg)
+
+		}
+	}
+
+	if m != nil && m.IsCommand() && m.Command() == "charade" {
 		log.Trace("charade trigger")
-		if active {
+		if userAct != nil {
 			userName, _ := bot.GetUN(m.From)
 			b.Send(tgbotapi.NewMessage(cm.ID, fmt.Sprintf(i18n.Get("Charade is in progress, go and win it, %s!", cm.Language), userName)))
 			return false, nil
 		}
 
-		err = c.startCharade(u, cm)
+		err = c.startCharade(m.From, cm)
 		if err != nil {
 			log.WithError(err).Error("cant start charade")
 		}
 
 		return false, nil
-	}
-
-	if active {
-		if u.CallbackQuery != nil && isValidCharadeCallback(u.CallbackQuery) {
-			log.Trace("charade valid")
-			answer := tgbotapi.CallbackConfig{
-				CallbackQueryID: u.CallbackQuery.ID,
-			}
-
-			switch u.CallbackQuery.Data {
-			case charadeShowWord:
-				answer.Text = i18n.Get("It's not your turn!", cm.Language)
-				if u.CallbackQuery.From.ID == c.active[cm.ID].userID {
-					answer.Text = c.active[cm.ID].word
-					answer.ShowAlert = true
-				}
-
-			case charadeOptOut:
-				answer.Text = i18n.Get("It's not your turn!", cm.Language)
-				if u.CallbackQuery.From.ID == c.active[cm.ID].userID {
-					delete(c.active, cm.ID)
-					answer.Text = i18n.Get("Ok then, you, coward.", cm.Language)
-				}
-			}
-
-			b.Request(answer)
-		}
 	}
 
 	return true, nil
@@ -119,41 +172,45 @@ func isValidCharadeCallback(query *tgbotapi.CallbackQuery) bool {
 	return res
 }
 
-func (c *Charades) startCharade(u *tgbotapi.Update, cm *db.ChatMeta) error {
+func (c *Charades) randomActForUser(userID int, cm *db.ChatMeta) (*act, error) {
 	if _, ok := c.data[cm.Language]; !ok {
 		if err := c.load(cm.Language); err != nil {
-			return errors.WithMessage(err, "")
+			return nil, errors.WithMessagef(err, "cant load charades for %v", cm.Language)
 		}
 	}
 
 	wordIndex := rand.Intn(len(c.data[cm.Language]))
 	var currentIndex int
-	var word, description string
-	for currentWord, currentDesc := range c.data[cm.Language] {
+	var word string
+	for currentWord := range c.data[cm.Language] {
 		if wordIndex == currentIndex {
 			word = currentWord
-			description = currentDesc
 			break
 		}
 		currentIndex++
 	}
-	_ = description // todo define what to do with desc
 
-	if word == "" {
-		return errors.New("cant pick word")
+	return &act{
+		userID:    userID,
+		word:      word,
+		startTime: func() *time.Time { t := time.Now(); return &t }(),
+	}, nil
+}
+
+func (c *Charades) startCharade(user *tgbotapi.User, cm *db.ChatMeta) error {
+	userAct, err := c.randomActForUser(user.ID, cm)
+	if err != nil {
+		return errors.WithMessage(err, "cant get act")
 	}
 
-	c.active[cm.ID] = act{
-		userID: u.Message.From.ID,
-		word:   word,
-	}
-	userName, _ := bot.GetUN(u.Message.From)
+	c.active[cm.ID] = userAct
+	userName, _ := bot.GetUN(user)
 	msg := tgbotapi.NewMessage(cm.ID, fmt.Sprintf(i18n.Get("You have one minute to explain word. Please, do not use sibling and other forms. Remember - both winner and explanator gets the point on success!\n\n*%s, press the button, to see the word, or to opt out.*", cm.Language), userName))
 	msg.ParseMode = "markdown"
 	kb := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData(i18n.Get("Show word", cm.Language), "charade_show_word"),
-			tgbotapi.NewInlineKeyboardButtonData(i18n.Get("I'm a chicken", cm.Language), "charade_opt_out"),
+			tgbotapi.NewInlineKeyboardButtonData(i18n.Get("💬 Show word", cm.Language), charadeShowWord),
+			tgbotapi.NewInlineKeyboardButtonData(i18n.Get("🔄 Replace word", cm.Language), charadeAnotherWord),
 		),
 	)
 	msg.ReplyMarkup = kb
