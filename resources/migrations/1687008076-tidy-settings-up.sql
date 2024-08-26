52239f50e8292af9488c6b9003b1f3990cdf1dc2
Valeriy Selitskiy <239034+iamwavecut@users.noreply.github.com>
2024-08-27 00:19:36 +0200

    debug: migrations

--- resources/migrations/1687008076-tidy-settings-up.sql
+++ resources/migrations/1687008076-tidy-settings-up.sql
@@ -1,12 +1,8 @@
 -- +migrate Up
 DROP TABLE IF EXISTS "charade_scores";
 DROP TABLE IF EXISTS "meta";
-
--- Check if columns exist before dropping
-PRAGMA foreign_keys=off;
-BEGIN TRANSACTION;
-
-ALTER TABLE "chats" RENAME TO "chats_old";
+DROP TABLE IF EXISTS "chat_members";
+DROP TABLE IF EXISTS "chats";
 
 CREATE TABLE "chats" (
     "id" INTEGER PRIMARY KEY,
@@ -14,16 +10,8 @@ CREATE TABLE "chats" (
     "migrated" BOOLEAN NOT NULL DEFAULT 0,
     "challenge_timeout" INTEGER NOT NULL DEFAULT 180,  -- 3 minutes in seconds
     "reject_timeout" INTEGER NOT NULL DEFAULT 600  -- 10 minutes in seconds
+    "language" TEXT NOT NULL DEFAULT 'en'
 );
-
-INSERT INTO "chats" (id, enabled, migrated, challenge_timeout, reject_timeout)
-SELECT id, 1, 0, 180, 600
-FROM "chats_old";
-
-DROP TABLE "chats_old";
-
-PRAGMA foreign_keys=on;
-COMMIT;
 
 CREATE TABLE IF NOT EXISTS "chat_members" (
     "chat_id" INTEGER NOT NULL,
@@ -33,24 +21,5 @@ CREATE TABLE IF NOT EXISTS "chat_members" (
 );
 
 -- +migrate Down
-DROP TABLE IF EXISTS "chat_members";
-
-PRAGMA foreign_keys=off;
-BEGIN TRANSACTION;
-
-ALTER TABLE "chats" RENAME TO "chats_old";
-
-CREATE TABLE "chats" (
-    "id" INTEGER PRIMARY KEY,
-    "title" TEXT,
-    "type" TEXT
-);
-
-INSERT INTO "chats" (id, title, type)
-SELECT id, NULL, NULL
-FROM "chats_old";
-
-DROP TABLE "chats_old";
-
-PRAGMA foreign_keys=on;
-COMMIT;
+-- no-op
+SELECT 1;
